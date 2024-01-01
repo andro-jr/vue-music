1cec930b056c7b486b6fbf04d3f1c402ce2a5a3f
andro-jr <prabin20panta@gmail.com>
2024-01-01 21:57:34 +0545

    setup store for auth

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -100,8 +100,7 @@
 </template>
 
 <script>
-import { auth, userCollection } from '@/includes/firebase';
-import { mapWritableState } from 'pinia';
+import { mapActions } from 'pinia';
 import useUserStore from '@/stores/user';
 
 export default {
@@ -126,20 +125,16 @@ export default {
       reg_alert_msg: 'Please wait! Your account is being created.'
     };
   },
-  computed: {
-    ...mapWritableState(useUserStore, ['userLoggedIn'])
-  },
   methods: {
+    ...mapActions(useUserStore, { createUser: 'register' }),
     async register(values) {
       this.reg_show_alert = true;
       this.reg_in_submittion = true;
       this.reg_alert_variant = 'bg-blue-500';
       this.reg_alert_msg = 'Please wait! Your account is being created.';
 
-      let userCredeantials = null;
-
       try {
-        userCredeantials = await auth.createUserWithEmailAndPassword(values.email, values.password);
+        await this.createUser(values);
       } catch (err) {
         console.log(err);
         this.reg_in_submittion = false;
@@ -148,25 +143,8 @@ export default {
         return;
       }
 
-      try {
-        await userCollection.add({
-          name: values.name,
-          email: values.email,
-          age: values.age,
-          country: values.country
-        });
-      } catch (err) {
-        console.log(err);
-        this.reg_in_submittion = false;
-        this.reg_alert_variant = 'bg-red-500';
-        this.reg_alert_msg = 'Unexpected errror occured. Please try again later!';
-      }
-
-      this.userLoggedIn = true;
-
       this.reg_alert_variant = 'bg-green-500';
       this.reg_alert_msg = 'Success! Your account has been created';
-      console.log(userCredeantials);
     }
   }
 };
--- src/stores/user.js
+++ src/stores/user.js
@@ -1,7 +1,29 @@
 import { defineStore } from 'pinia';
+import { auth, userCollection } from '@/includes/firebase';
 
 export default defineStore('user', {
   state: () => ({
     userLoggedIn: false
-  })
+  }),
+  actions: {
+    async register(values) {
+      const userCredentials = await auth.createUserWithEmailAndPassword(
+        values.email,
+        values.password
+      );
+
+      await userCollection.doc(userCredentials.user.uid).set({
+        name: values.name,
+        email: values.email,
+        age: values.age,
+        country: values.country
+      });
+
+      await userCredentials.user.updateProfile({
+        displayName: values.name
+      });
+
+      this.userLoggedIn = true;
+    }
+  }
 });
