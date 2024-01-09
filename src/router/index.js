69401c3e321c0490746e479ac5fe40f54ad5edec
andro-jr <prabin20panta@gmail.com>
2024-01-09 21:54:26 +0545

    router setup

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,56 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeViewVue from '@/views/HomeView.vue';
+import AboutViewVue from '@/views/AboutView.vue';
+import ManageViewVue from '@/views/ManageView.vue';
+import useUserStore from '@/stores/user';
+
+const routes = [
+  {
+    name: 'home',
+    path: '/',
+    component: HomeViewVue
+  },
+  {
+    name: 'about',
+    path: '/about',
+    component: AboutViewVue
+  },
+  {
+    name: 'manage',
+    path: '/manage-music',
+    component: ManageViewVue,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'manage' }
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' }
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  if (!to.meta.requiresAuth) {
+    return next();
+  }
+
+  const store = useUserStore();
+
+  if (store.userLoggedIn) {
+    next();
+  } else {
+    next({ name: 'home' });
+  }
+});
+
+export default router;
