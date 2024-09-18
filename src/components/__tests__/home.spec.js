6231f3760c71b8efcde9f1d1bf40153029e95568
andro-jr <prabin20panta@gmail.com>
2024-09-18 23:29:48 +0545

    test: mocking methods

--- src/components/__tests__/home.spec.js
+++ src/components/__tests__/home.spec.js
@@ -1,21 +1,37 @@
 import HomeView from '@/views/HomeView.vue';
 import { shallowMount } from '@vue/test-utils';
-import { describe, expect, test } from 'vitest';
-import SongItem from '../SongItem.vue';
-describe('Home.vue', () => {
-  test('renders song list', () => {
-    const songs = [{}, {}, {}];
+import { describe, expect, test, vi } from 'vitest';
+import SongItem from '@/components/SongItem.vue';
 
+vi.mock('@/includes/firebase', () => {
+  return {
+    songsCollection: {
+      doc: () => ({ get: () => Promise.resolve() }),
+      startAfter: () => ({
+        limit: () => ({
+          get: () => Promise.resolve([])
+        })
+      })
+    }
+  };
+});
+
+describe('Home.vue', () => {
+  test('renders list of songs', () => {
+    const songs = [{}, {}, {}];
     const component = shallowMount(HomeView, {
       data() {
         return {
           songs
         };
+      },
+      global: {
+        mocks: {
+          $t: (message) => message
+        }
       }
     });
-
     const items = component.findAllComponents(SongItem);
-
     expect(items).toHaveLength(songs.length);
   });
 });
