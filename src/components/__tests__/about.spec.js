4b17d71717fe31268b98b87aad6abd1adaab3e1a
andro-jr <prabin20panta@gmail.com>
2024-09-18 22:49:34 +0545

    test: shallow mount used instead of mount

--- src/components/__tests__/about.spec.js
+++ src/components/__tests__/about.spec.js
@@ -1,10 +1,10 @@
 import AboutView from '@/views/AboutView.vue';
-import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import { describe, expect, test } from 'vitest';
 
 describe('About.vue', () => {
   test('renders inner text', () => {
-    const wrapper = mount(AboutView);
+    const wrapper = shallowMount(AboutView);
 
     expect(wrapper.text()).toContain('about');
   });
